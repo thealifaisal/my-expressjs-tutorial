f11706b7c39f488ca086aca1d2998858da3574b5
Ali Faisal <alifaisalas99@gmail.com>
2021-01-24 17:31:21 +0500

    init

---
+++ index.js
@@ -0,0 +1,61 @@
+// CommonJS format for importing packages.
+// var express = require('express');
+// var bodyParser = require('body-parser');
+// var cookieParser = require('cookie-parser');
+
+// ES6 format for importing modules
+import express from 'express';
+
+// Since body-parser is CommonJS module and not ES6 module,
+// it can always be imported via the default export, for example using:
+// $ import pkg from 'body-parser';
+// $ const { urlencoded, json } = pkg;
+// otherwise for using:
+// $ import { urlencoded, json } from 'body-parser';
+// Syntax Error is thrown
+import pkg from 'body-parser';
+const { urlencoded, json } = pkg;
+
+// ES6 format for importing modules
+import cookieParser from 'cookie-parser';
+
+// above ES6 format gives syntax error:
+// Cannot use import statement outside a module
+// the solution is to add: {"type": "module"} in package.json
+// but this is also giving error because, node version is 12
+// ES6 imports are allowed in node version 13 or greater.
+// So updating to node 13 or greater, which is 14.15.4 LTS
+
+var app = express();
+
+// using bodyParser populates the body of the request, otherwise body is unidentified
+// app.use(bodyParser()); // Deprecated
+
+// using cookieParser populates the cookies of the request, req.cookies, otherwise cookie is unidentified
+app.use(cookieParser());
+
+// since the above constructor is deprecated, bodyParser(),
+// the below commands are required to run seperately
+app.use(urlencoded({extended: true}));
+app.use(json());
+
+// require router defined in movies.js
+import movies from './movies.js';
+
+// use the router on the sub-route /movies
+// eg: when client a sends a request using, localhost:3000/movies,
+// the request will be re-routed to ./movies.js file which is referenced
+// by movies variable 
+app.use('/movies', movies);
+
+// when client a sends a request using, localhost:3000/
+// below handler will run
+app.get('/', function(req, res){
+    console.log('Welcome to index.js');
+    res.status(200).send('Welcome to index.js');
+});
+
+// the node app listens for client requests on port 3000
+app.listen(3000);
+
+console.log('server listening on port 3000');
---
+++ movies.js
@@ -0,0 +1,36 @@
+import { Router } from 'express';
+
+// creating a router
+var router = Router();
+
+// ES6 format
+export default router;
+
+// CommonJS format
+// modules.export = router;
+
+// after client's request, localhost:3000/movies, gets re=routed to movies.js file,
+// the request is inside the movies.js file and http methods such as, get, post, delete, or put,
+// would process the request by using the route '/' which currently is the root aka /movies/
+router.get('/', function(req, res){
+	res.status(200).send('Return some movies');
+});
+
+// further to fetch a specific movie with ID, the route will be '/:id'
+// e.g: router.get('/:id', function(req, res){})
+// but to ensure the format of the ID, a regular expression is used with id,
+// e.g: /:id([0-9]{3,}) => number with digits 0-9 and length >= 3
+router.get('/:id([0-9]{3,})', function(req, res){
+	res.status(200).send(req.params.id);
+});
+
+// when ID is sent in wrong format, error is returned as a response,
+// Cannot GET /movies/12
+// this happens because the request url did not matched with any of the handlers above.
+// this would work if we define a new handler that would take an ID, without restricting the format
+router.get('/:id', function(req, res){
+	res.status(200).send(`boo hoo i, ID: ${req.params.id}, dont need any restrictions, you sucker handlers at the top`);
+	// note: string interpolation, ${req.params.id}, works when backticks are used, ``, and not single quotations, ''.
+});
+// ...and it worked!
+
